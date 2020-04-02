7005e7728cc2f11270479ce01aef9cc8f80c745a
Gyubin Lee <leegyubin00@gmail.com>
2020-04-02 16:24:11 +0900

    v1.0.0

---
+++ Assignment_#1/20190439_assign1/wc209.c
@@ -0,0 +1,143 @@
+/*Gyubin Lee, Assignment 1, wc209.c*/
+
+#include <stdio.h>
+#include <ctype.h>
+#include <assert.h>
+#include <stdlib.h>
+
+/*global variables 'line', 'word', 'charac' indicates 
+the number of line, word, character. 'temp'is use for indicating
+the line where '/*' start*/
+
+int line = 1; int word = 0; int charac = 0; int temp = 1;
+
+//define 7 state of DFA
+enum DFAState {OUT, IN, A_1, A_2, B, C, D};
+
+//if c is '\n', increase line and charac
+void newline(void){
+	line += 1;
+	charac += 1;
+}
+
+//if isspace(c) is true, this function will be called
+void space(int c){
+	if (c == '\n') newline();
+	else charac +=1;
+}
+
+int main(void){
+	enum DFAState state = OUT; //Initial state of DFA is 'O'
+	int c; //get character from input stream
+
+	/*executing DFA by following while loop until there is
+	no character in the input stream. This loop calculate the
+	number of lines, words, and characters by increasing, or
+	decreasing the global variable 'line', 'word' and 'charac'*/
+
+	while((c = getchar()) != EOF){
+		switch(state){
+			case OUT:
+				if(isspace(c)){
+					space(c); break;
+				} else if(c == '/'){ 
+					word += 1; charac += 1;
+					state = A_2; break;
+				} else{
+					word += 1; charac += 1;
+					state = IN; break;
+				}
+			case IN:
+				if(isspace(c)){
+					space(c);
+					state = OUT; break;
+				} else if(c == '/'){
+					charac += 1;
+					state = A_1; break;
+				} else{
+					charac += 1; break;
+				}
+			case A_1:
+				if(isspace(c)){
+					space(c);
+					state = OUT; break;
+				} else if(c == '*'){
+					charac -= 1;
+					temp = line;
+					state = B; break;
+				} else if(c == '/'){
+					charac += 1; break;
+				} else{
+					charac += 1;
+					state = IN; break;
+				}
+			case B:
+				if(c == '*'){
+					state = C; break;
+				} else{
+					if(c == '\n') newline();
+					break;
+				}
+			case C:
+				if(c == '/'){
+					charac += 1;
+					state = D; break;
+				} else if (c == '*'){
+					break;
+				}
+				else{
+					if(c == '\n') newline();
+					state = B; break;
+				}
+			case D:
+				if(isspace(c)){
+					space(c);
+					state = OUT; break;
+				} else if(c == '/'){
+					charac += 1;
+					word += 1;
+					state = A_2; break;
+				} else{
+					word += 1;
+					charac += 1;
+					state = IN; break;
+				}
+			case A_2:
+				if(isspace(c)){
+					space(c);
+					state = OUT; break;
+				} else if(c == '*'){
+					charac -= 1;
+					word -= 1;
+					temp = line;
+					state = B; break;
+				} else if(c == '/'){
+					charac += 1; break;
+				} else{
+					charac += 1;
+					state = IN; break;
+				}
+			default:
+				assert(0); break;
+			}
+	}
+	
+	/*if charac == 0, it means that there is nothing
+	in the input stream. So, 'line' should be 0*/
+
+	if(charac == 0) {
+		line = 0;
+	};
+
+	/*state B and C means that the comment is unterminated. So we
+	should throw the error in these two cases. Else, print the
+	result in standard output stream*/
+
+	if(state == B || state == C){
+		fprintf(stderr, "Error: line %d: unterminated comment\n", temp);		
+		return EXIT_FAILURE;	
+	} else {
+		printf("%d %d %d\n", line, word, charac);
+		return EXIT_SUCCESS;
+	}
+}
---
+++ Assignment_#1/20190439_assign1/wc209.s
@@ -0,0 +1,414 @@
+	.file	"wc209.c"
+	.text
+	.globl	line
+	.data
+	.align 4
+	.type	line, @object
+	.size	line, 4
+line:
+	.long	1
+	.globl	word
+	.bss
+	.align 4
+	.type	word, @object
+	.size	word, 4
+word:
+	.zero	4
+	.globl	charac
+	.align 4
+	.type	charac, @object
+	.size	charac, 4
+charac:
+	.zero	4
+	.globl	temp
+	.data
+	.align 4
+	.type	temp, @object
+	.size	temp, 4
+temp:
+	.long	1
+	.text
+	.globl	newline
+	.type	newline, @function
+newline:
+.LFB6:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movl	line(%rip), %eax
+	addl	$1, %eax
+	movl	%eax, line(%rip)
+	movl	charac(%rip), %eax
+	addl	$1, %eax
+	movl	%eax, charac(%rip)
+	nop
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE6:
+	.size	newline, .-newline
+	.globl	space
+	.type	space, @function
+space:
+.LFB7:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$8, %rsp
+	movl	%edi, -4(%rbp)
+	cmpl	$10, -4(%rbp)
+	jne	.L3
+	call	newline
+	jmp	.L5
+.L3:
+	movl	charac(%rip), %eax
+	addl	$1, %eax
+	movl	%eax, charac(%rip)
+.L5:
+	nop
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE7:
+	.size	space, .-space
+	.section	.rodata
+.LC0:
+	.string	"wc209.c"
+.LC1:
+	.string	"0"
+	.align 8
+.LC2:
+	.string	"Error: line %d: unterminated comment\n"
+.LC3:
+	.string	"%d %d %d\n"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB8:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$16, %rsp
+	movl	$0, -8(%rbp)
+	jmp	.L7
+.L34:
+	cmpl	$6, -8(%rbp)
+	ja	.L39
+	movl	-8(%rbp), %eax
+	leaq	0(,%rax,4), %rdx
+	leaq	.L10(%rip), %rax
+	movl	(%rdx,%rax), %eax
+	cltq
+	leaq	.L10(%rip), %rdx
+	addq	%rdx, %rax
+	notrack jmp	*%rax
+	.section	.rodata
+	.align 4
+	.align 4
+.L10:
+	.long	.L16-.L10
+	.long	.L15-.L10
+	.long	.L14-.L10
+	.long	.L13-.L10
+	.long	.L12-.L10
+	.long	.L11-.L10
+	.long	.L9-.L10
+	.text
+.L16:
+	call	__ctype_b_loc@PLT
+	movq	(%rax), %rax
+	movl	-4(%rbp), %edx
+	movslq	%edx, %rdx
+	addq	%rdx, %rdx
+	addq	%rdx, %rax
+	movzwl	(%rax), %eax
+	movzwl	%ax, %eax
+	andl	$8192, %eax
+	testl	%eax, %eax
+	je	.L17
+	movl	-4(%rbp), %eax
+	movl	%eax, %edi
+	call	space
+	jmp	.L7
+.L17:
+	cmpl	$47, -4(%rbp)
+	jne	.L18
+	movl	word(%rip), %eax
+	addl	$1, %eax
+	movl	%eax, word(%rip)
+	movl	charac(%rip), %eax
+	addl	$1, %eax
+	movl	%eax, charac(%rip)
+	movl	$3, -8(%rbp)
+	jmp	.L7
+.L18:
+	movl	word(%rip), %eax
+	addl	$1, %eax
+	movl	%eax, word(%rip)
+	movl	charac(%rip), %eax
+	addl	$1, %eax
+	movl	%eax, charac(%rip)
+	movl	$1, -8(%rbp)
+	jmp	.L7
+.L15:
+	call	__ctype_b_loc@PLT
+	movq	(%rax), %rax
+	movl	-4(%rbp), %edx
+	movslq	%edx, %rdx
+	addq	%rdx, %rdx
+	addq	%rdx, %rax
+	movzwl	(%rax), %eax
+	movzwl	%ax, %eax
+	andl	$8192, %eax
+	testl	%eax, %eax
+	je	.L19
+	movl	-4(%rbp), %eax
+	movl	%eax, %edi
+	call	space
+	movl	$0, -8(%rbp)
+	jmp	.L7
+.L19:
+	cmpl	$47, -4(%rbp)
+	jne	.L20
+	movl	charac(%rip), %eax
+	addl	$1, %eax
+	movl	%eax, charac(%rip)
+	movl	$2, -8(%rbp)
+	jmp	.L7
+.L20:
+	movl	charac(%rip), %eax
+	addl	$1, %eax
+	movl	%eax, charac(%rip)
+	jmp	.L7
+.L14:
+	call	__ctype_b_loc@PLT
+	movq	(%rax), %rax
+	movl	-4(%rbp), %edx
+	movslq	%edx, %rdx
+	addq	%rdx, %rdx
+	addq	%rdx, %rax
+	movzwl	(%rax), %eax
+	movzwl	%ax, %eax
+	andl	$8192, %eax
+	testl	%eax, %eax
+	je	.L21
+	movl	-4(%rbp), %eax
+	movl	%eax, %edi
+	call	space
+	movl	$0, -8(%rbp)
+	jmp	.L7
+.L21:
+	cmpl	$42, -4(%rbp)
+	jne	.L22
+	movl	charac(%rip), %eax
+	subl	$1, %eax
+	movl	%eax, charac(%rip)
+	movl	line(%rip), %eax
+	movl	%eax, temp(%rip)
+	movl	$4, -8(%rbp)
+	jmp	.L7
+.L22:
+	cmpl	$47, -4(%rbp)
+	jne	.L23
+	movl	charac(%rip), %eax
+	addl	$1, %eax
+	movl	%eax, charac(%rip)
+	jmp	.L7
+.L23:
+	movl	charac(%rip), %eax
+	addl	$1, %eax
+	movl	%eax, charac(%rip)
+	movl	$1, -8(%rbp)
+	jmp	.L7
+.L12:
+	cmpl	$42, -4(%rbp)
+	jne	.L24
+	movl	$5, -8(%rbp)
+	jmp	.L7
+.L24:
+	cmpl	$10, -4(%rbp)
+	jne	.L7
+	call	newline
+	jmp	.L7
+.L11:
+	cmpl	$47, -4(%rbp)
+	jne	.L26
+	movl	charac(%rip), %eax
+	addl	$1, %eax
+	movl	%eax, charac(%rip)
+	movl	$6, -8(%rbp)
+	jmp	.L7
+.L26:
+	cmpl	$42, -4(%rbp)
+	jne	.L27
+	jmp	.L7
+.L27:
+	cmpl	$10, -4(%rbp)
+	jne	.L28
+	call	newline
+.L28:
+	movl	$4, -8(%rbp)
+	jmp	.L7
+.L9:
+	call	__ctype_b_loc@PLT
+	movq	(%rax), %rax
+	movl	-4(%rbp), %edx
+	movslq	%edx, %rdx
+	addq	%rdx, %rdx
+	addq	%rdx, %rax
+	movzwl	(%rax), %eax
+	movzwl	%ax, %eax
+	andl	$8192, %eax
+	testl	%eax, %eax
+	je	.L29
+	movl	-4(%rbp), %eax
+	movl	%eax, %edi
+	call	space
+	movl	$0, -8(%rbp)
+	jmp	.L7
+.L29:
+	cmpl	$47, -4(%rbp)
+	jne	.L30
+	movl	charac(%rip), %eax
+	addl	$1, %eax
+	movl	%eax, charac(%rip)
+	movl	word(%rip), %eax
+	addl	$1, %eax
+	movl	%eax, word(%rip)
+	movl	$3, -8(%rbp)
+	jmp	.L7
+.L30:
+	movl	word(%rip), %eax
+	addl	$1, %eax
+	movl	%eax, word(%rip)
+	movl	charac(%rip), %eax
+	addl	$1, %eax
+	movl	%eax, charac(%rip)
+	movl	$1, -8(%rbp)
+	jmp	.L7
+.L13:
+	call	__ctype_b_loc@PLT
+	movq	(%rax), %rax
+	movl	-4(%rbp), %edx
+	movslq	%edx, %rdx
+	addq	%rdx, %rdx
+	addq	%rdx, %rax
+	movzwl	(%rax), %eax
+	movzwl	%ax, %eax
+	andl	$8192, %eax
+	testl	%eax, %eax
+	je	.L31
+	movl	-4(%rbp), %eax
+	movl	%eax, %edi
+	call	space
+	movl	$0, -8(%rbp)
+	jmp	.L7
+.L31:
+	cmpl	$42, -4(%rbp)
+	jne	.L32
+	movl	charac(%rip), %eax
+	subl	$1, %eax
+	movl	%eax, charac(%rip)
+	movl	word(%rip), %eax
+	subl	$1, %eax
+	movl	%eax, word(%rip)
+	movl	line(%rip), %eax
+	movl	%eax, temp(%rip)
+	movl	$4, -8(%rbp)
+	jmp	.L7
+.L32:
+	cmpl	$47, -4(%rbp)
+	jne	.L33
+	movl	charac(%rip), %eax
+	addl	$1, %eax
+	movl	%eax, charac(%rip)
+	jmp	.L7
+.L33:
+	movl	charac(%rip), %eax
+	addl	$1, %eax
+	movl	%eax, charac(%rip)
+	movl	$1, -8(%rbp)
+	jmp	.L7
+.L39:
+	leaq	__PRETTY_FUNCTION__.2985(%rip), %rcx
+	movl	$121, %edx
+	leaq	.LC0(%rip), %rsi
+	leaq	.LC1(%rip), %rdi
+	call	__assert_fail@PLT
+.L7:
+	call	getchar@PLT
+	movl	%eax, -4(%rbp)
+	cmpl	$-1, -4(%rbp)
+	jne	.L34
+	movl	charac(%rip), %eax
+	testl	%eax, %eax
+	jne	.L35
+	movl	$0, line(%rip)
+.L35:
+	cmpl	$4, -8(%rbp)
+	je	.L36
+	cmpl	$5, -8(%rbp)
+	jne	.L37
+.L36:
+	movl	temp(%rip), %edx
+	movq	stderr(%rip), %rax
+	leaq	.LC2(%rip), %rsi
+	movq	%rax, %rdi
+	movl	$0, %eax
+	call	fprintf@PLT
+	movl	$1, %eax
+	jmp	.L38
+.L37:
+	movl	charac(%rip), %ecx
+	movl	word(%rip), %edx
+	movl	line(%rip), %eax
+	movl	%eax, %esi
+	leaq	.LC3(%rip), %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	movl	$0, %eax
+.L38:
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE8:
+	.size	main, .-main
+	.section	.rodata
+	.type	__PRETTY_FUNCTION__.2985, @object
+	.size	__PRETTY_FUNCTION__.2985, 5
+__PRETTY_FUNCTION__.2985:
+	.string	"main"
+	.ident	"GCC: (Ubuntu 9.2.1-9ubuntu2) 9.2.1 20191008"
+	.section	.note.GNU-stack,"",@progbits
+	.section	.note.gnu.property,"a"
+	.align 8
+	.long	 1f - 0f
+	.long	 4f - 1f
+	.long	 5
+0:
+	.string	 "GNU"
+1:
+	.align 8
+	.long	 0xc0000002
+	.long	 3f - 2f
+2:
+	.long	 0x3
+3:
+	.align 8
+4:
